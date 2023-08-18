52374ea29e8c2eb40d1691efd75f4af3d1f2db51
shahed <shahed.rahman88@gmail.com>
2023-08-18 14:49:55 +0600

    dish cardd design done

--- src/components/body/Menu.js
+++ src/components/body/Menu.js
@@ -1,17 +1,21 @@
 import { useState } from "react"
 import MenuItem from "./MenuItem"
 import DISHES from "../../data/dishes"
+import DishDetail from "./DishDetail"
 
 const Menu = () => {
     const [dishes] = useState(DISHES)
     const menu = dishes.map(dish => {
-        return <MenuItem key={dish.id} menu={dish} />
+        return <MenuItem key={dish.id} dish={dish} />
     })
   return (
     <div className="container">
         <div className="row">
             <div className="col-5">
                 {menu}   
+            </div>
+            <div className="col-7">
+                <DishDetail /> 
             </div>
         </div>
     </div>
--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,6 +1,33 @@
-const MenuItem = ({menu}) => {
+import { Card, CardImg, CardImgOverlay, CardText, CardTitle } from "reactstrap"
+
+const MenuItem = ({dish}) => {
   return (
-    <div>{menu.name}</div>
+    <div>
+      <Card inverse style={{padding: "10px", margin: "10px"}}>
+        <CardImg
+          alt={dish.name}
+          src={dish.image}
+          style={{
+            height: 270,
+            opacity: 0.8
+          }}
+          width="100%"
+        />
+        <CardImgOverlay>
+          <CardTitle tag="h2" style={{fontWeight: "bold", color: "#252B48"}}>
+            {dish.name}
+          </CardTitle>
+          <CardText>
+            {dish.description}
+          </CardText>
+          <CardText>
+            <small className="text-muted">
+              Last updated 3 mins ago
+            </small>
+          </CardText>
+        </CardImgOverlay>
+      </Card>
+    </div>
   )
 }
 
