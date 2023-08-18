a0fcfbb7622c5a8f0b720efe18b1d8f06fae6dc7
shahed <shahed.rahman88@gmail.com>
2023-08-18 21:09:19 +0600

    code refactor, collapse nav added

--- src/components/body/BodyComponent.js
+++ src/components/body/BodyComponent.js
@@ -1,10 +1,7 @@
-import React from 'react'
-import Menu from './Menu'
-
 const BodyComponent = () => {
   return (
     <div>
-      <Menu />
+      
     </div>
   )
 }
--- src/components/header/NavigationComponent.js
+++ src/components/header/NavigationComponent.js
@@ -1,13 +1,35 @@
-import React from 'react'
-import { Navbar, NavbarBrand } from 'reactstrap'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Navbar, NavbarBrand, Collapse, Nav, NavItem, NavbarToggler } from 'reactstrap'
 
 const NavigationComponent = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false)
+  const toggleNavbar = () => {
+    setIsNavOpen(!isNavOpen)
+  }
   return (
     <div>
-        <Navbar color="dark" dark >
+        <Navbar color="dark" dark expand="sm">
+          <NavbarToggler onClick={toggleNavbar}/>
             <NavbarBrand href="/">
                 ReactRestaurant
             </NavbarBrand>
+          <Collapse isOpen={isNavOpen} navbar>
+            <Nav className="me-auto" navbar>
+              <NavItem>
+                <Link to="/">Home</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/about">About</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/menu">Menu</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/contact">Contact</Link>
+              </NavItem>
+            </Nav>
+          </Collapse>
         </Navbar>
     </div>
   )
